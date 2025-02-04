680d1b27937669dcbec09affe1c9b3b24abcd016
jdiazrv <54633503+jdiazrv@users.noreply.github.com>
2025-02-04 18:34:17 +0100

    dele old files

--- config.js
+++
@@ -1,9 +0,0 @@
-{
-  "anchorUpdateInterval": 30,
-  "electricUpdateInterval": 30,
-  "engineUpdateInterval": 600,
-  "watermakerUpdateInterval": 100,
-  "weatherUpdateInterval": 120,
-  "deepSleepStart": "23:00",
-  "deepSleepEnd": "06:30"
-}
