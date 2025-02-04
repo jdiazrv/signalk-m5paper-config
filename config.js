6854e51bc1fb280496e82ca80b1e5ad2ac3ce4f8
jdiazrv <54633503+jdiazrv@users.noreply.github.com>
2025-02-04 18:24:34 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+{
+  "anchorUpdateInterval": 30,
+  "electricUpdateInterval": 30,
+  "engineUpdateInterval": 600,
+  "watermakerUpdateInterval": 100,
+  "weatherUpdateInterval": 120,
+  "deepSleepStart": "23:00",
+  "deepSleepEnd": "06:30"
+}
